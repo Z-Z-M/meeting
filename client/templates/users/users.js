99a484e3e11a7067e7705b9578ec3a8e52b8916f
Z-Z-M <zuozhi1987@gmail.com>
2015-05-29 10:59:42 +0800

    Delete users.js
    delete users.js

--- client/templates/users/users.js
+++
@@ -1,8 +0,0 @@
-  Template.users.events({
-  	'click #code': function () {
-      $('#code-text').val('8888');
-    },
-    'click #submit': function () {
-      window.location = '../account';
-    }
-  });
